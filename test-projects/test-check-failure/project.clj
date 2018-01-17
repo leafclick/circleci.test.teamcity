1852402149772875d8bea6f123434994d8ae7252
Kamil Toman <kamil.toman@leafclick.com>
2018-01-17 18:46:52 +0100

    Bump dev version up to 0.2.0-SNAPSHOT.

--- test-projects/test-check-failure/project.clj
+++ test-projects/test-check-failure/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.leafclick/circleci.test.teamcity "0.1.0"]]
+                 [com.leafclick/circleci.test.teamcity "0.2.0-SNAPSHOT"]]
   :monkeypatch-clojure-test false
   :aliases {"test" ["run" "-m" "circleci.test/dir" :project/test-paths]
             "tests" ["run" "-m" "circleci.test"]
